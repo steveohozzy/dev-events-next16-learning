808872fdc0e2c336dd1e527534a4544c66671b39
STEVEN_HOSKINS <steve.hoskins@theentertainer.com>
2025-11-18 16:08:04 +0000

    fix cache

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,16 @@
 'use client'
 
+import { cacheLife } from "next/cache";
+
 import { useEffect } from "react"
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  'use cache';
+  cacheLife('hours');
+
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
